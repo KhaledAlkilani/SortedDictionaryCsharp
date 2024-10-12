124037ce21df74663dfe5cfdfa6ab7e55f342f26
KhaledAlkilani <khaledkelany@gmail.com>
2024-10-12 12:44:29 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace SortedDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, int> students = new Dictionary<string, int>();
+
+            while (true)
+            {
+                Console.WriteLine("Enter a studen name:");
+                string studentName = Console.ReadLine() ?? throw new Exception("Student name cannot be empty.");
+
+                if (string.IsNullOrWhiteSpace(studentName))
+                {
+                    break;
+
+                } 
+
+                Console.WriteLine("Enter student grade:");
+                string studentGrade = Console.ReadLine() ?? throw new Exception(null);
+
+                if (int.TryParse(studentGrade, out int grade))
+                {
+                    students[studentName] = grade;
+                } else
+
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            }
+
+            var sortedDictionary = students.OrderBy(x => x.Key);
+
+            foreach (var student in sortedDictionary)
+            {
+                Console.WriteLine($"Student name: {student.Key}, Student grade: {student.Value}");
+            }
+        }
+    }
+}
+
